1a9f6e104c4bbc8afb8468427ea8197de2390cc4
bouzuya <m@bouzuya.net>
2018-08-13 21:22:41 +0900

    add Bouzuya.HTTP.Client

---
+++ src/Bouzuya/HTTP/Client.js
@@ -0,0 +1,67 @@
+"use strict";
+
+exports.fetchImpl = function (options) {
+  return function () {
+    var http = require('http');
+    var https = require('https');
+    var url = require('url');
+    return new Promise(function (resolve, reject) {
+      var urlObj = url.parse(options.url);
+      var request = (urlObj.protocol === 'https:' ? https : http).request({
+        family: urlObj.family,
+        headers: options.headers,
+        host: urlObj.host,
+        method: options.method,
+        path: urlObj.path,
+        port: urlObj.port,
+        protocol: urlObj.protocol,
+      }, function (response) {
+        // TODO: http.IncomingMessage Event 'aborted'
+        // TODO: http.IncomingMessage Event 'close'
+        var body = [];
+        response.setEncoding('utf8'); // TODO: support Buffer
+        // TODO: stream.Readable Event 'close'
+        response.on('data', function (chunk) {
+          body += chunk;
+        });
+        response.on('end', function () {
+          resolve({ statusCode: response.statusCode, body: body });
+        });
+        response.on('error', function (error) {
+          reject(error);
+        });
+        // ignore stream.Readable Event 'readable'
+      });
+      // TODO: Event 'abort'
+      // TODO: Event 'connect'
+      // TODO: Event 'continue' 100
+      // TODO: Event 'information' 1xx
+      // Event 'response' See: callback
+      // TODO: Event 'socket'
+      // TODO: Event 'timeout'
+      // TODO: Event 'upgrade'
+      // TODO: stream.Writable Event 'close'
+      // TODO: stream.Writable Event 'drain'
+      request.on('error', function (error) {
+        reject(error);
+      });
+      // TODO: stream.Writable Event 'finish'
+      // ignore stream.Writable Event 'pipe'
+      // ignore stream.Writable Event 'unpipe'
+      if (typeof options.body !== 'undefined') {
+        request.write(options.body);
+      }
+      request.end();
+    });
+  };
+};
+
+exports.textImpl = function (response) {
+  return typeof response.body !== 'string'
+    ? JSON.stringify(response.body)
+    : response.body;
+};
+
+exports.statusImpl = function (response) {
+  return response.statusCode;
+};
---
+++ src/Bouzuya/HTTP/Client.purs
@@ -0,0 +1,60 @@
+module Bouzuya.HTTP.Client
+  ( FetchOptions
+  , body
+  , defaults
+  , fetch
+  , headers
+  , method
+  , url
+  ) where
+
+import Bouzuya.HTTP.Method (Method)
+import Bouzuya.HTTP.Method as Method
+import Control.Promise (Promise)
+import Control.Promise as Promise
+import Data.Functor.Contravariant (cmap)
+import Data.Maybe (Maybe(..))
+import Data.Options (Option, Options, defaultToOptions, opt, options)
+import Effect (Effect)
+import Effect.Aff (Aff)
+import Effect.Class (liftEffect)
+import Foreign (Foreign)
+import Foreign.Object (Object)
+import Prelude (append, bind, eq, pure, show)
+
+data FetchOptions
+data FetchResponse
+
+foreign import fetchImpl :: Foreign -> Effect (Promise FetchResponse)
+
+foreign import textImpl :: FetchResponse -> String
+
+foreign import statusImpl :: FetchResponse -> Int
+
+body :: Option FetchOptions String
+body = opt "body"
+
+defaults :: Options FetchOptions
+defaults = defaultToOptions "method" Method.GET
+
+headers :: Option FetchOptions (Object String)
+headers = opt "headers"
+
+method :: Option FetchOptions Method
+method = cmap show (opt "method")
+
+url :: Option FetchOptions String
+url = opt "url"
+
+fetch :: Options FetchOptions -> Aff { body :: Maybe String, status :: Int }
+fetch opts = do
+  promise <- liftEffect (fetchImpl (options (append defaults opts)))
+  response <- Promise.toAff promise
+  let status = statusImpl response
+  pure
+    { body:
+        if eq status 204
+          then Nothing
+          else Just (textImpl response)
+    , status
+    }
