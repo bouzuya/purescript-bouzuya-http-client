4df5a6f760185f77f892897161469135ea5d60b3
bouzuya <m@bouzuya.net>
2018-08-14 11:27:46 +0900

    Main -> Test.Main

--- test/Main.purs
+++ test/Main.purs
@@ -1,16 +1,15 @@
-module Main (main) where
+module Test.Main (main) where
 
 import Bouzuya.HTTP.Client (fetch, url)
 import Data.Options ((:=))
 import Effect (Effect)
 import Effect.Aff (launchAff_)
 import Effect.Class (liftEffect)
-import Effect.Console (log, logShow)
+import Effect.Console (logShow)
 import Prelude (Unit, bind, discard)
 
 main :: Effect Unit
 main = launchAff_ do
-  liftEffect (log "Hello")
   let options = url := "https://bouzuya.net/"
   { body, status } <- fetch options
   liftEffect (logShow body)
